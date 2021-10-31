b40ade8368f0484a8fd993883267c93944f98f48
hosono kotaro <hosono1985@gmail.com>
2021-11-01 01:20:44 +0900

    feat: Add plugins, Change import sort

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { VFC } from 'react';
 import { AppProps } from 'next/app';
+import { VFC } from 'react';
 
 // NOTE: App 全体を拡張するために利用します。
 const App: VFC<AppProps> = ({ Component, pageProps }) => {
