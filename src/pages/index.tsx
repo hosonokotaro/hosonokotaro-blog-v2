168b0495d31dadb4d71f9567ec1392431fdb04fc
hosono kotaro <hosono1985@gmail.com>
2021-11-09 23:41:56 +0900

    fix: Get API response

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
+import { InferGetStaticPropsType } from 'next';
+import { VFC } from 'react';
+
 import Text from '@/atoms/Text';
 
-const HomePage = () => {
+type PostTitleDate = {
+  id: string;
+  title: string;
+  release: boolean;
+  createDate: string;
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch(`${process.env.API_ENDPOINT}/get/titlelist`);
+  const posts: PostTitleDate[] = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Top: VFC<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  posts,
+}) => {
   return (
     <div>
-      <Text />
+      {posts.map((post) => (
+        <Text key={post.id} text={post.title} />
+      ))}
     </div>
   );
 };
 
-export default HomePage;
+export default Top;
