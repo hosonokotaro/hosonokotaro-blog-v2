62532c49d1d2c593b1074b5f31592bd31cb75e0b
hosono kotaro <hosono1985@gmail.com>
2021-12-11 20:24:12 +0900

    feat: Add getPost

---
+++ src/services/getPost.ts
@@ -0,0 +1,43 @@
+import axios from '~/adapter/axios';
+
+const endpoint = (id: string) => {
+  return {
+    default: `${process.env.API_ENDPOINT}/get/post/${id}`,
+    private: `${process.env.API_ENDPOINT}/get/post/${id}?private=enabled`,
+  };
+};
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+  release: boolean;
+  createDate: string;
+};
+
+export type Target = keyof ReturnType<typeof endpoint>;
+
+const getPost = async (
+  id: string,
+  target: Target = 'default',
+  idToken?: string
+) => {
+  let headers: { Authorization?: string } = {};
+
+  if (target === 'private' && idToken) {
+    headers = { Authorization: `Bearer ${idToken}` };
+  }
+
+  try {
+    const { data } = await axios.get<Post>(endpoint(id)[target], {
+      headers,
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default getPost;
