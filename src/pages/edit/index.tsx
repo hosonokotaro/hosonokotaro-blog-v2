82daf2f86e44f154de074244dada93bdf1f4581c
hosono kotaro <hosono1985@gmail.com>
2021-12-12 01:34:14 +0900

    feat: Add titleList hooks

--- src/pages/edit/index.tsx
+++ src/pages/edit/index.tsx
@@ -10,48 +10,42 @@ import TextBox from '@/atoms/TextBox';
 import Title from '@/atoms/Title';
 import Layout from '@/layout';
 import useSession from '~/customHooks/useSession';
-
-type TitleDate = {
-  id: string;
-  title: string;
-  release: boolean;
-  createDate: string;
-};
+import useTitleList from '~/customHooks/useTitleList';
 
 const Edit: VFC = () => {
-  const { userId, login, logout } = useSession();
-
-  // TODO: CSR にする為に、useEditTop を実装する
-  const titleDateList: TitleDate[] = [];
+  const { idToken, login, logout, userId } = useSession();
+  const titleList = useTitleList(idToken);
 
   return (
-    <Layout title="Edit" isPrivate>
-      <PageLayout tagName="article">
-        <Title text="全記事一覧" />
-        {titleDateList.map(({ id, title, createDate }) => (
-          <ContentBox key={id} marginTopSize="40px">
-            <Link href={`/${id}`}>
-              <a>
-                <Title rank="span" text={title} />
-              </a>
-            </Link>
-            <Date text={createDate} />
-          </ContentBox>
-        ))}
-        {!titleDateList.length && (
-          <ContentBox marginTopSize="40px">
-            <ErrorMessage text="記事一覧が取得できません" />
-          </ContentBox>
-        )}
-      </PageLayout>
+    <>
+      <Layout title="Edit" isPrivate>
+        <PageLayout tagName="article">
+          <Title text="全記事一覧" />
+          {titleList.map(({ id, title, createDate }) => (
+            <ContentBox key={id} marginTopSize="40px">
+              <Link href={`/${id}`}>
+                <a>
+                  <Title rank="span" text={title} />
+                </a>
+              </Link>
+              <Date text={createDate} />
+            </ContentBox>
+          ))}
+          {!titleList.length && (
+            <ContentBox marginTopSize="40px">
+              <ErrorMessage text="記事一覧が取得できません" />
+            </ContentBox>
+          )}
+        </PageLayout>
+      </Layout>
       <ContentBox textAlign="center">
         {userId && <Button text="ログアウトする" handleClick={logout} />}
         {!userId && <Button text="ログインする" handleClick={login} />}
         <ContentBox marginTopSize="20px" textAlign="center">
-          <TextBox>uid: {userId}</TextBox>
+          <TextBox>{userId ? `uid: ${userId}` : 'No login'}</TextBox>
         </ContentBox>
       </ContentBox>
-    </Layout>
+    </>
   );
 };
 
