02deaf4709c546bd4efb1d080cecab64052c68dd
hosono kotaro <hosono1985@gmail.com>
2021-12-18 18:51:14 +0900

    fix: Add mock setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,18 @@ import '~/style/global.css';
 import { AppProps } from 'next/app';
 import { VFC } from 'react';
 
+const setup = async () => {
+  if (typeof window === 'undefined') {
+    const { server } = await import('~/mocks/server');
+    server.listen();
+  } else {
+    const { worker } = await import('~/mocks/browser');
+    worker.start();
+  }
+};
+
+setup();
+
 // NOTE: App 全体を拡張するために利用します。
 const App: VFC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
