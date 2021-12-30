0f0c4429a3b4c763d4949b50f9600cae39b9ec17
hosono kotaro <hosono1985@gmail.com>
2021-12-30 11:00:11 +0900

    fix: PostId の型解決を services に移動

--- src/customHooks/usePost.ts
+++ src/customHooks/usePost.ts
@@ -1,18 +1,10 @@
 import useSWR from 'swr';
 
 import { IdToken } from '~/services/authentication';
-import getPost from '~/services/getPost';
+import getPost, { PostId } from '~/services/getPost';
 
-const usePost = (postId: string | string[] = '', idToken: IdToken) => {
-  let resolveId = '';
-
-  // NOTE: useRouter の query が array の場合は、空文字にしておく
-  if (!Array.isArray(postId)) {
-    resolveId = postId;
-  }
-
-  // TODO: fetch 頻度を設定する
-  const { data, error } = useSWR([resolveId, idToken], getPost);
+const usePost = (postId: PostId, idToken: IdToken) => {
+  const { data, error } = useSWR([postId, idToken], getPost);
 
   return { post: data, isLoading: !error && !data, isError: error };
 };
--- src/services/getPost.ts
+++ src/services/getPost.ts
@@ -1,4 +1,5 @@
 import axios from '~/adapter/axios';
+import { IdToken } from '~/services/authentication';
 
 const endpoint = (id: string) => {
   return {
@@ -6,6 +7,10 @@ const endpoint = (id: string) => {
     private: `/get/post/${id}?is_unixtime_format=enabled&private=enabled`,
   };
 };
+
+export type Target = keyof ReturnType<typeof endpoint>;
+// NOTE: Next.js の ParsedUrlQuery で指定される型
+export type PostId = string | string[] | undefined;
 
 export type Post = {
   id: string;
@@ -15,12 +20,12 @@ export type Post = {
   createDate: string;
 };
 
-export type Target = keyof ReturnType<typeof endpoint>;
+const getPost = async (postId: PostId, idToken?: IdToken) => {
+  if (!postId || Array.isArray(postId)) return;
 
-const getPost = async (id: string, idToken?: string) => {
   try {
     const { data } = await axios.get<Post>(
-      endpoint(id)[idToken ? 'private' : 'default'],
+      endpoint(postId)[idToken ? 'private' : 'default'],
       idToken ? { headers: { Authorization: `Bearer ${idToken}` } } : {}
     );
 
