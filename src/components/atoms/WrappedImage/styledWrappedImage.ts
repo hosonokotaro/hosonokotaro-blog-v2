e0b4eb99b48202abc43d0a9e0572141b5e5a7a91
hosono kotaro <hosono1985@gmail.com>
2022-01-17 20:23:54 +0900

    fix: Add image width, height

--- src/components/atoms/WrappedImage/styledWrappedImage.ts
+++ src/components/atoms/WrappedImage/styledWrappedImage.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledWrappedImage = styled.img`
+  width: auto;
+  height: auto;
   max-width: 100%;
   box-shadow: 0px 0px 20px -5px #ddd;
 `;
