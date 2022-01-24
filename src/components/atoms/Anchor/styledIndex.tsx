5741fabffd5bd2f63c0dfda9efc448d10096e240
hosono kotaro <hosono1985@gmail.com>
2022-01-25 00:27:26 +0900

    fix: Change Anchor params

--- src/components/atoms/Anchor/styledIndex.tsx
+++ src/components/atoms/Anchor/styledIndex.tsx
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
-export const StyledAnchor = styled.a`
-  color: #d33;
-
-  &:after {
-    font-size: 0.8rem;
-    content: '（外部サイト）';
-  }
-`;
-
 export const StyledLink = styled.a`
   color: #d33;
 `;
