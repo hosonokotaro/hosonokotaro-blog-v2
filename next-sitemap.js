c57b5a0258e679a812692edf21ef04ca2376b60d
hosono kotaro <hosono1985@gmail.com>
2022-04-24 21:06:06 +0900

    fix: development environment

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.LOCAL_SITE_URL || 'https://techblog.hosonokotaro.jp',
+  siteUrl: 'https://techblog.hosonokotaro.jp',
   generateRobotsTxt: true,
   exclude: ['/edit', '/edit/*'],
 };
