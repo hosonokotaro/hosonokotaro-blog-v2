181c01386e0f78f0a249e557db371c2ac5fee589
hosono kotaro <hosono1985@gmail.com>
2022-07-23 20:23:28 +0900

    fix: title style

--- src/components/atoms/SiteTitle/index.tsx
+++ src/components/atoms/SiteTitle/index.tsx
@@ -1,23 +1,22 @@
-import { VFC } from 'react';
+import { StyledInlineBlock, StyledSiteTitle } from './styledIndex';
 
-import { StyledInlineBlock } from './styledIndex';
-
-interface Props {
+type Props = {
   text1: string;
   text2: string;
   text3: string;
-}
+};
 
-const SiteTitle: VFC<Partial<Props>> = ({
+const SiteTitle = ({
   text1 = 'HOSONO',
   text2 = 'KOTARO',
   text3 = 'Tech Blog',
-}) => {
+}: Partial<Props>) => {
   return (
-    <h1>
+    <StyledSiteTitle>
       <StyledInlineBlock>{text1}</StyledInlineBlock>
-      <StyledInlineBlock>{text2}</StyledInlineBlock> {text3}
-    </h1>
+      <StyledInlineBlock>{text2}</StyledInlineBlock>
+      <StyledInlineBlock>{text3}</StyledInlineBlock>
+    </StyledSiteTitle>
   );
 };
 
--- src/components/atoms/SiteTitle/styledIndex.ts
+++ src/components/atoms/SiteTitle/styledIndex.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const StyledSiteTitle = styled.h1`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
 export const StyledInlineBlock = styled.span`
   display: inline-block;
+  margin: 0 4px;
 `;
