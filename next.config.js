11e847a7ec096de5c1f4effc18ed38378e32f318
hosono kotaro <hosono1985@gmail.com>
2022-07-26 00:16:33 +0900

    feat: Update React 18

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /**
- * @type { import('next').NextConfig }
- **/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // NOTE: client 側に渡せる環境変数
-  env: {
-    // USE_MSW: process.env.USE_MSW,
-  },
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
       (rule) => rule.test && rule.test.test('.svg')
@@ -28,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins([withBundleAnalyzer], nextConfig);
+module.exports = nextConfig;
