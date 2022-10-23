0b6e688fb9c5142b55226cc24caaa35ffb253ad6
hosono kotaro <hosono1985@gmail.com>
2022-10-23 13:52:49 +0900

    feat: Development environment settings for SVG icon implementation

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
   env: {
     // USE_MSW: process.env.USE_MSW,
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    );
+
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
+  },
 };
 
 module.exports = withPlugins([withBundleAnalyzer], nextConfig);
