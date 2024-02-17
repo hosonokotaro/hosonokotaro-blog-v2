776b5c48bd8fb886481e9afa3b2f329b4a116eb2
hosonokotaro <hosono1985@gmail.com>
2024-02-17 17:39:53 +0900

    fix: Page layout styles

--- src/components/atoms/PageLayout/index.css.ts
+++ src/components/atoms/PageLayout/index.css.ts
@@ -1,10 +1,7 @@
-import { createVar, style } from '@vanilla-extract/css';
-
-export const minHeight = createVar();
+import { style } from '@vanilla-extract/css';
 
 export const base = style({
   maxWidth: '1000px',
   margin: '80px auto',
-  minHeight,
   padding: '0 40px',
 });
